9495a829f496358abf1918e71ddb9bd8536ba9eb
Mr-Lordmetal <lordmetal@msn.com>
2021-06-25 15:06:21 +0200

    The last time?

--- src/QuizEnd.js
+++ src/QuizEnd.js
@@ -2,14 +2,14 @@ import React, { Component } from 'react';
 
 class QuizEnd extends Component {
   handleResetClick() {
-    this.setState({ quizPosition: 1 });
+    this.props.resetClickHandler();
   }
 
   render() {
     return (
       <div>
         <p>Thanks for playing!</p>
-        <a href='' onclick={ this.handleResetClick.bind(this) }>Reset Quiz</a>
+        <a href='#' onClick={ this.handleResetClick.bind(this) }>Reset Quiz</a>
       </div>
     );
   }
